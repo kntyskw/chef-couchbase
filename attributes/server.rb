93d860cffdada48f7b35acab5b361f7842dd0957
Kenta Yasukawa <kentay@amazon.com>
2013-02-23 09:32:21 +0900

    added rpm

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,9 +1,16 @@
 package_machine = node['kernel']['machine'] == "x86_64" ? "x86_64" : "x86"
 
-default['couchbase']['server']['edition'] = "community"
-default['couchbase']['server']['version'] = "1.8.0"
+case node[:platform]
+when 'ubuntu','debian'
+  packaging = deb
+when 'centos','redhat','fedora','amazon'
+  packaging = rpm
+end
 
-default['couchbase']['server']['package_file'] = "couchbase-server-#{node['couchbase']['server']['edition']}_#{package_machine}_#{node['couchbase']['server']['version']}.deb"
+default['couchbase']['server']['edition'] = "community"
+default['couchbase']['server']['version'] = "2.0.0"
+
+default['couchbase']['server']['package_file'] = "couchbase-server-#{node['couchbase']['server']['edition']}_#{package_machine}_#{node['couchbase']['server']['version']}.#{packaging}"
 default['couchbase']['server']['package_base_url'] = "http://packages.couchbase.com/releases/#{node['couchbase']['server']['version']}"
 default['couchbase']['server']['package_full_url'] = "#{node['couchbase']['server']['package_base_url']}/#{node['couchbase']['server']['package_file']}"
 
