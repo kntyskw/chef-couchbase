96da2b023b4331b86bca2e7ec8e7b4eca976f9ad
Kenta Yasukawa <kenta.yasukawa@gmail.com>
2013-04-12 07:42:06 +0000

    fixed chef 11 compatibility

--- libraries/bucket_resource.rb
+++ libraries/bucket_resource.rb
@@ -5,46 +5,57 @@ class Chef
   class Resource
     class CouchbaseBucket < Resource
       include Couchbase::CredentialsAttributes
+
+      def self.method_missing(meth, *args, &block)
+        if meth.to_s =~ /^attribute$/
+          self.__attribute(args[0], args[1])
+        else
+          super
+        end
+      end
+
+      def self.__attribute(attr_name, validation_opts={})
+        class_eval(<<-SHIM, __FILE__, __LINE__)
+                def #{attr_name}(arg=nil)
+                _set_or_return_#{attr_name}(arg)
+                end
+        SHIM
+
+        define_method("_set_or_return_#{attr_name.to_s}".to_sym) do |arg|
+          set_or_return(attr_name.to_sym, arg, validation_opts)
+        end
+      end
+
+
+      attribute :bucket, :kind_of => String, :name_attribute => true
+      attribute :cluster, :kind_of => String, :default => "default"
+      attribute :exists, :kind_of => [TrueClass, FalseClass], :required => true
+
+      attribute :memory_quota_mb, :kind_of => Integer, :callbacks => {
+          "must be at least 100" => lambda { |quota| quota >= 100 },
+        }
+
+      attribute :memory_quota_percent, :kind_of => Numeric, :callbacks => {
+          "must be a positive number" => lambda { |percent| percent > 0.0 },
+          "must be less than or equal to 1.0" => lambda { |percent| percent <= 1.0 },
+        }
+
+      attribute :replicas, :kind_of => [Integer, FalseClass], :default => 1, :callbacks => {
+          "must be a non-negative integer" => lambda { |replicas| !replicas || replicas > -1 },
+        }
+
+      attribute :type, :kind_of => String, :default => "couchbase", :callbacks => {
+           "must be either couchbase or memcached" => lambda { |type| %w(couchbase memcached).include? type },
+         }
+
       def initialize(*)
         super
         @action = :create
         @allowed_actions.push :create
 	@resource_name = :couchbase_bucket
 
-        self.class.attribute :bucket, :kind_of => String, :name_attribute => true
-        self.class.attribute :cluster, :kind_of => String, :default => "default"
-        self.class.attribute :exists, :kind_of => [TrueClass, FalseClass], :required => true
-
-        self.class.attribute :memory_quota_mb, :kind_of => Integer, :callbacks => {
-          "must be at least 100" => lambda { |quota| quota >= 100 },
-        }
-
-        self.class.attribute :memory_quota_percent, :kind_of => Numeric, :callbacks => {
-          "must be a positive number" => lambda { |percent| percent > 0.0 },
-          "must be less than or equal to 1.0" => lambda { |percent| percent <= 1.0 },
-        }
-
-        self.class.attribute :replicas, :kind_of => [Integer, FalseClass], :default => 1, :callbacks => {
-          "must be a non-negative integer" => lambda { |replicas| !replicas || replicas > -1 },
-        }
-
-        self.class.attribute :type, :kind_of => String, :default => "couchbase", :callbacks => {
-           "must be either couchbase or memcached" => lambda { |type| %w(couchbase memcached).include? type },
-         }
 
       end
-      def self.attribute(attr_name, validation_opts={})
-        class_eval(<<-SHIM, __FILE__, __LINE__)
-		def #{attr_name}(arg=nil)
-		_set_or_return_#{attr_name}(arg)
-		end
-	SHIM
-
-        define_method("_set_or_return_#{attr_name.to_s}".to_sym) do |arg|
-          set_or_return(attr_name.to_sym, arg, validation_opts)
-        end
-      end
-
     end
   end
 end
--- libraries/cluster_resource.rb
+++ libraries/cluster_resource.rb
@@ -6,6 +6,29 @@ class Chef
     class CouchbaseCluster < Resource
       include Couchbase::CredentialsAttributes
 
+      def self.method_missing(meth, *args, &block)
+        if meth.to_s =~ /^attribute$/
+          self.__attribute(args[0], args[1])
+        else
+          super
+        end
+      end
+
+      def self.__attribute(attr_name, validation_opts={})
+        class_eval(<<-SHIM, __FILE__, __LINE__)
+                def #{attr_name}(arg=nil)
+                _set_or_return_#{attr_name}(arg)
+                end
+        SHIM
+
+        define_method("_set_or_return_#{attr_name.to_s}".to_sym) do |arg|
+          set_or_return(attr_name.to_sym, arg, validation_opts)
+        end
+      end
+
+      attribute :cluster, :kind_of => String, :name_attribute => true
+      attribute :exists, :kind_of => [TrueClass, FalseClass], :required => true
+      attribute :memory_quota_mb, :kind_of => Integer, :required => true, :callbacks => { "must be at least 256" => lambda { |quota| quota >= 256 } }
 
       def initialize(*)
         super
@@ -13,30 +36,7 @@ class Chef
         @allowed_actions.push :create_if_missing
         @resource_name = :couchbase_cluster
 
-        self.class.attribute :cluster, :kind_of => String, :name_attribute => true
-        self.class.attribute :exists, :kind_of => [TrueClass, FalseClass], :required => true
-        self.class.attribute :memory_quota_mb, :kind_of => Integer, :required => true, :callbacks => {
-        "must be at least 256" => lambda { |quota| quota >= 256 }
-      }
-
       end
-
-	# Define an attribute on this resource, including optional validation
-	# parameters.
-        def self.attribute(attr_name, validation_opts={})
-            # Ruby 1.8 doesn't support default arguments to blocks, but we have to
-            # use define_method with a block to capture +validation_opts+.
-            # Workaround this by defining two methods :(
-            class_eval(<<-SHIM, __FILE__, __LINE__)
-                def #{attr_name}(arg=nil)
-                    _set_or_return_#{attr_name}(arg)
-                end
-            SHIM
-
-            define_method("_set_or_return_#{attr_name.to_s}".to_sym) do |arg|
-                set_or_return(attr_name.to_sym, arg, validation_opts)
-            end
-        end
     end
   end
 end
